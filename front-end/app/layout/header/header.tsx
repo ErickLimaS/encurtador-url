e6d02e40b16574aac27a9c23ce961bb4d34889f8
Erick Lima da Silva <ericklima63@gmail.com>
2023-06-21 13:47:27 -0300

    Front-end: Header Css Tweaks

--- front-end/app/layout/header/header.tsx
+++ front-end/app/layout/header/header.tsx
@@ -25,72 +25,75 @@ export default function Header() {
     }, [])
 
     return (
-        <header id={styles.container}>
+        <header id={styles.header_tag}>
 
-            <Link href={'/'}>Brand Logo</Link>
+            <div id={styles.container}>
 
-            <nav className={styles.desktop_only}>
-                <ul className={styles.row} id={styles.nav_list_container}>
-                    <li><Link href={'/p/top-100'}>Top 100</Link></li>
-                    <li><Link href={'/p/docs'}>Documentação</Link></li>
-                </ul>
-            </nav>
+                <Link href={'/'}>Brand Logo</Link>
 
-            <div id={styles.user_panel_container}>
+                <nav className={styles.desktop_only}>
+                    <ul className={styles.row} id={styles.nav_list_container}>
+                        <li><Link href={'/p/top-100'}>Top 100</Link></li>
+                        <li><Link href={'/p/docs'}>Documentação</Link></li>
+                    </ul>
+                </nav>
 
-                <button
-                    id={styles.menu_panel_btn}
-                    aria-controls={styles.menu_list_panel}
-                    onClick={() => {
-                        setIsUserPanelExpanded(!isUserPanelExpanded)
-                    }}
-                >
-                    <SVG.List aria-label={isUserPanelExpanded ? "Fechar Menu" : "Abrir Menu"}/>
-                </button>
+                <div id={styles.user_panel_container}>
 
-                <div aria-expanded={isMenuExpanded} id={styles.menu_list_panel}>
                     <button
-                        className={styles.desktop_only}
-                        id={styles.user_open_panel_btn}
-                        aria-controls={styles.user_panel}
+                        id={styles.menu_panel_btn}
+                        aria-controls={styles.menu_list_panel}
                         onClick={() => {
                             setIsUserPanelExpanded(!isUserPanelExpanded)
                         }}
                     >
-                        {isUserPanelExpanded ? (
-                            <><SVG.XCircle alt="Cruz Indicada para Fechar Menu." />Fechar Menu</>
-                        ) : (
-                            <><SVG.PersonFill alt="Perfil de Pessoa." />Meu Perfil</>
-                        )}
-
+                        <SVG.List aria-label={isUserPanelExpanded ? "Fechar Menu" : "Abrir Menu"} />
                     </button>
 
-                    <div aria-expanded={isUserPanelExpanded} id={styles.user_panel}>
+                    <div aria-expanded={isMenuExpanded} id={styles.menu_list_panel}>
+                        <button
+                            className={styles.desktop_only}
+                            id={styles.user_open_panel_btn}
+                            aria-controls={styles.user_panel}
+                            onClick={() => {
+                                setIsUserPanelExpanded(!isUserPanelExpanded)
+                            }}
+                        >
+                            {isUserPanelExpanded ? (
+                                <><SVG.XCircle alt="Cruz Indicada para Fechar Menu." />Fechar Menu</>
+                            ) : (
+                                <><SVG.PersonFill alt="Perfil de Pessoa." />Meu Perfil</>
+                            )}
 
-                        <h5 className={styles.mobile_only}><SVG.PersonFill /> Usuário</h5>
+                        </button>
 
-                        <ul role='menu'>
-                            <li>
-                                <Link href={'/user/activities'} role='menuitem'><SVG.ClockHistory />Histórico</Link>
-                            </li>
-                            <li>
-                                <button role='menuitem'><SVG.BoxArrowLeft />Sair</button>
-                            </li>
-                        </ul>
+                        <div aria-expanded={isUserPanelExpanded} id={styles.user_panel}>
 
-                        <h5 className={styles.mobile_only}>Links</h5>
+                            <h5 className={styles.mobile_only}><SVG.PersonFill /> Usuário</h5>
 
-                        <ul role='menu' className={styles.mobile_only}>
-                            <li role='menuitem'>
-                                <Link href={'/p/top-100'}>Top 100</Link>
-                            </li>
-                            <li role='menuitem'>
-                                <Link href={'/p/docs'}>Documentação</Link>
-                            </li>
-                        </ul>
+                            <ul role='menu'>
+                                <li>
+                                    <Link href={'/user/activities'} role='menuitem'><SVG.ClockHistory />Histórico</Link>
+                                </li>
+                                <li>
+                                    <button role='menuitem'><SVG.BoxArrowLeft />Sair</button>
+                                </li>
+                            </ul>
+
+                            <h5 className={styles.mobile_only}>Links</h5>
+
+                            <ul role='menu' className={styles.mobile_only}>
+                                <li role='menuitem'>
+                                    <Link href={'/p/top-100'}>Top 100</Link>
+                                </li>
+                                <li role='menuitem'>
+                                    <Link href={'/p/docs'}>Documentação</Link>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
 
+                </div>
             </div>
 
         </header>
